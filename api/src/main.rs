9d889e208f651781fb1a238a5da35398970bd0aa
natnael-eyuel-dev <natnael.eyuel.dev@gmail.com>
2025-10-14 10:17:00 +0300

    chore: clone the database_url inside main

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,7 +6,7 @@ use metta_kg::{db, rocket};
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let cli = metta_kg::cli::Cli::parse();
-    db::init_database_url(cli.database_url);
+    db::init_database_url(cli.database_url.clone());
     let rocket_instance = rocket(&cli).await;
     rocket_instance.launch().await?;
 
