708d970d29fe280587e44757bdb1396446752766
DagmawiKK <dagmawikassa1@gmail.com>
2025-11-22 18:01:17 +0300

    refactor: define int type based on db

--- api/src/model.rs
+++ api/src/model.rs
@@ -3,6 +3,12 @@ use chrono::NaiveDateTime;
 use diesel::{Insertable, Queryable, QueryableByName, Selectable};
 use rocket::serde::{Deserialize, Serialize};
 
+#[cfg(feature = "sqlite")]
+type DbInt = i32;
+
+#[cfg(feature = "postgres")]
+type DbInt = i32;
+
 #[derive(Serialize, Deserialize, Insertable, Clone)]
 #[diesel(table_name = tokens)]
 pub struct TokenInsert {
@@ -15,13 +21,15 @@ pub struct TokenInsert {
     pub permission_share_share: bool,
     pub permission_share_read: bool,
     pub permission_share_write: bool,
-    pub parent: Option<i32>,
+    pub parent: Option<DbInt>,
 }
 
 #[derive(Serialize, Deserialize, Queryable, Selectable, Clone, QueryableByName)]
 #[diesel(table_name = tokens)]
+#[cfg_attr(feature = "sqlite", diesel(check_for_backend(diesel::sqlite::Sqlite)))]
+#[cfg_attr(feature = "postgres", diesel(check_for_backend(diesel::pg::Pg)))]
 pub struct Token {
-    pub id: i32,
+    pub id: DbInt,
     pub code: String,
     pub description: String,
     pub namespace: String,
@@ -31,5 +39,5 @@ pub struct Token {
     pub permission_share_share: bool,
     pub permission_share_read: bool,
     pub permission_share_write: bool,
-    pub parent: Option<i32>,
+    pub parent: Option<DbInt>,
 }
